fe9621fc684a7f0f67ad33076074bbbff1c4edd5
segur <segur.opus@gmail.com>
2020-05-26 13:56:10 +0900

    Added TestScriptEditModeWithAsm.cs

---
+++ Assets/WithAsm/TestsEditModeWithAsm/TestScriptEditModeWithAsm.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+namespace Tests
+{
+    public class TestScriptEditModeWithAsm
+    {
+        [Test]
+        public void MyClassTest1()
+        {
+            var x = new MyClassWithAsm();
+            Assert.IsEmpty(x.GetName());
+            x.SetName("Alice");
+            Assert.AreEqual("Alice", x.GetName());
+        }
+
+        [Test]
+        public void MyClassTest2()
+        {
+            var x = new MyClassWithAsm("Bob");
+            Assert.AreEqual("Bob", x.GetName());
+        }
+    }
+}
