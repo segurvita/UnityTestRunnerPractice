54bea77f3d59c59700810ba78a3e8bda4d2519df
segur <segur.opus@gmail.com>
2020-05-26 13:56:25 +0900

    Added MyClassWithoutAsm.cs

---
+++ Assets/WithouAsm/Scripts/MyClassWithoutAsm.cs
@@ -0,0 +1,24 @@
+﻿public class MyClassWithoutAsm
+{
+    private string name;
+
+    public MyClassWithoutAsm()
+    {
+        name = string.Empty;
+    }
+
+    public MyClassWithoutAsm(string name)
+    {
+        this.name = name;
+    }
+
+    public void SetName(string name)
+    {
+        this.name = name;
+    }
+
+    public string GetName()
+    {
+        return name;
+    }
+}
