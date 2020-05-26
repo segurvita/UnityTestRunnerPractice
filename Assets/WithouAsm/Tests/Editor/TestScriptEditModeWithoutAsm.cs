0946be99011ee7c2f5439a973f67929215f8546f
segur <segur.opus@gmail.com>
2020-05-26 13:56:44 +0900

    Added TestScriptEditModeWithoutAsm.cs

---
+++ Assets/WithouAsm/Tests/Editor/TestScriptEditModeWithoutAsm.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class TestScriptEditModeWithoutAsm
+    {
+        [Test]
+        public void MyClassTest1()
+        {
+            var x = new MyClassWithoutAsm();
+            Assert.IsEmpty(x.GetName());
+            x.SetName("Alice");
+            Assert.AreEqual("Alice", x.GetName());
+        }
+
+        [Test]
+        public void MyClassTest2()
+        {
+            var x = new MyClassWithoutAsm("Bob");
+            Assert.AreEqual("Bob", x.GetName());
+        }
+    }
+}
