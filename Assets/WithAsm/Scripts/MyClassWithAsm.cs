6754e60bb6d6983b5b25d80d1c71cd5cf408c528
segur <segur.opus@gmail.com>
2020-05-26 13:55:32 +0900

    Added MyClassWithAsm.cs

---
+++ Assets/WithAsm/Scripts/MyClassWithAsm.cs
@@ -0,0 +1,24 @@
+﻿public class MyClassWithAsm
+{
+    private string name;
+
+    public MyClassWithAsm()
+    {
+        name = string.Empty;
+    }
+
+    public MyClassWithAsm(string name)
+    {
+        this.name = name;
+    }
+
+    public void SetName(string name)
+    {
+        this.name = name;
+    }
+
+    public string GetName()
+    {
+        return name;
+    }
+}
